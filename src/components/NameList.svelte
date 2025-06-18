80dd7bec004fe2ca88bbed1aa10979ce93a06074
PratikPatil3235 <pratikpatil@medianv.com>
2025-06-18 13:19:46 +0530

    NameList.svelte added

---
+++ src/components/NameList.svelte
@@ -0,0 +1,22 @@
+<script>
+  const names = [
+    {
+      firstName: "Bruce",
+      lastName: "Wane",
+    },
+    {
+      firstName: "Clark",
+      lastName: "kent",
+    },
+    {
+      firstName: "Berry",
+      lastName: "Allen",
+    },
+  ];
+</script>
+
+<main>
+    {#each names as name (name) }
+        <h3><slot name="hero" firstName={name.firstName} lastName={name.lastName}/></h3>
+    {/each}
+</main>
